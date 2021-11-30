2445c8d0374a743d3331c68ad523bd72c27626f0
trevorwrightdev <trevor.wright.710@gmail.com>
2021-11-30 12:22:21 -0700

    Animated the progress bars and subtracted hunger, tiredness, and boredom values every second

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
-
 // On start
+let hunger = 25;
+let tiredness = 30;
+let boredom = 35;
+
 $('.title-menu').animate({bottom: 0}, {duration: 800, queue: false, complete: () => {
 
     $('.play-button').on('click', playButton);
@@ -26,10 +29,21 @@ let playButton = (e) => {
 let play = () => {
     // if we want to add random tamagotchi functionality, we just need to have no image initially
     // and then assign a random one right here
-
-    // decrease the values over time. I wonder if this can be done asyncronously. probably.
     
+    // * Animate progress bars
+    $('#hunger').animate({width: 0}, hunger * 1000);
+    $('#tiredness').animate({width: 0}, tiredness * 1000);
+    $('#boredom').animate({width: 0}, boredom * 1000);
 
+    // * Decrease stat values every second, as if it matched up with the animations
 
+    setInterval(() => {
+        hunger--;
+        tiredness--;
+        boredom--;
+
+        // TODO: Check for zeroes to make the player lose        
+
+    }, 1000);
 
 }
